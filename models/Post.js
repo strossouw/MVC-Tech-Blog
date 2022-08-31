bb831f0e7366f15ae09efbf5e830a5a8a32aa562
SRoss <srossouw@uwm.edu>
2022-08-31 10:01:56 -0500

    changed variables

--- models/Post.js
+++ models/Post.js
@@ -1,37 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 // create our Post model
-class Post extends Model {
-  static upvote(body, models) {
-    return models.Vote.create({
-      user_id: body.user_id,
-      post_id: body.post_id
-    }).then(() => {
-      return Post.findOne({
-        where: {
-          id: body.post_id
-        },
-        attributes: [
-          'id',
-          'post_url',
-          'title',
-          'created_at',
-          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
-          {
-            model: models.Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: models.User,
-              attributes: ['username']
-            }
-          }
-        ]
-      });
-    });
-  }
-}
+class Post extends Model {}
 
 // create fields/columns for Post model
 Post.init(
@@ -46,13 +16,10 @@ Post.init(
       type: DataTypes.STRING,
       allowNull: false
     },
-    post_url: {
+    post_content: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isURL: true
-      }
-    },
+            allowNull: false,
+        },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -65,7 +32,8 @@ Post.init(
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post'
+    modelName: 'post',
+    onDelete: 'cascade'
   }
 );
 
